86efd743b1f7d9d76c4ad95166e5daaa1c3bfcc9
jsxgod <smykoo7@gmail.com>
2021-11-18 11:27:02 +0100

    Clean up imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export {default as Header } from "./Header";
 export {default as LinkHistory } from "./LinkHistory";
+export {default as Gallery } from "./Gallery";
 export {default as Product } from "./Product";
 export {default as Details } from "./Details";
