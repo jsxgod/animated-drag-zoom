6369fdabab77e830c94f7bf88c69afaddc4439e8
jsxgod <smykoo7@gmail.com>
2021-11-20 06:56:17 +0100

    Add database connection to the server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const connectToDatabase = require("./config/db");
+
+connectToDatabase();
 
 const app = express();
+
+app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
 
