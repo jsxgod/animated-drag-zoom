72ce4c5fa2bb270350edc2083a113f1373fce78e
jsxgod <smykoo7@gmail.com>
2021-11-20 06:57:09 +0100

    Mock first product

---
+++ backend/data/products.js
@@ -0,0 +1,48 @@
+const products = [
+  {
+    product_type: "SkateBoard",
+    brand: "Toy Machine",
+    name: "Cat Monster Complete",
+    description:  "Take a ride and start rollin right with the Toy Machine Cat Monster Complete 8.25 x 31.88. This is a high quality complete that's perfect for beginning skateboarders, smaller riders, or yougnsters looking to start skateboarding. Features a top quality Toy Machine 7-ply maple deck, Rukus trucks, Toy Machine 52mm wheels and shielded Toy Machine abec 5 bearings. Fully assembled and ready to ride right out of the box.",
+    price: 99,
+    images: {
+      large: "https://imgur.com/4jwWKS6",
+      back: "https://imgur.com/2TV1sqM",
+      closeUp: "https://imgur.com/UNqoruT",
+      side: "https://imgur.com/bndKHjZ",
+      wheel: "https://imgur.com/aJBCo33",
+    },
+    details: [
+      {
+        label: "Width",
+        value: "8.25"
+      },
+      {
+        label: "Length",
+        value: "31.88"
+      },
+      {
+        label: "Wheels",
+        value: "Toy Machine 52mm"
+      },
+      {
+        label: "Wheelbase",
+        value: "14"
+      },
+      {
+        label: "Deck",
+        value: "Toy Machine 7-ply maple deck"
+      },
+      {
+        label: "Trucks",
+        value: "Rukus"
+      },
+      {
+        label: "Bearings",
+        value: "Shielded Pig abec 5"
+      }
+    ]
+  },
+];
+
+module.exports = products;
