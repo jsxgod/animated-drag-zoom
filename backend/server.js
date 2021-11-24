4c233f5c79189adca5ee0cd797d8c09b1bb14a07
jsxgod <smykoo7@gmail.com>
2021-11-24 15:20:10 +0100

    Use the pproducts API on the server. Use cors.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const connectToDatabase = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
 
 connectToDatabase();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
