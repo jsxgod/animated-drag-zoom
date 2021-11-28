2cc80efbddc257580dd3a8321ebd8a4b37a6e2e4
jsxgod <smykoo7@gmail.com>
2021-11-28 22:14:04 +0100

    Add cart icon. Animate the hamburger button. Make use of the Redux store.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,72 @@
 import React from "react";
 import Logo from "../assets/infinity-logo.png";
+import { FaShoppingCart } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { Menu } from ".";
+import { motion } from "framer-motion";
+import { closeMenu, toggleMenu } from "../redux/features/menu/menuSlice";
 
 const Header = () => {
+  const cartCounter = useSelector((state) => state.cart.totalQuantity);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const menuOpened = useSelector((state) => state.menu.opened);
+
+  const handleNavigate = (route) => {
+    dispatch(closeMenu());
+    navigate(route);
+  };
+
   return (
-    <div className="header">
-      <div className="header-wrapper">
-        <div className="header-content">
-          <div className="logo-wrapper">
-            <img src={Logo} alt="Logo" />
-          </div>
-          <div className="hamburger-menu">
-            <span></span>
-            <span></span>
-            <span></span>
+    <>
+      <div className="header">
+        <div className="header-wrapper">
+          <div className="header-content">
+            <div className="logo-wrapper">
+              <img src={Logo} alt="Logo" onClick={() => handleNavigate("/")} />
+            </div>
+            <div className="header-buttons-container">
+              <div
+                className="cart-button-wrapper"
+                onClick={() => handleNavigate("/cart")}
+              >
+                <span className="cart-counter">{cartCounter}</span>
+                <FaShoppingCart className="cart-icon" />
+              </div>
+              <button
+                className="hamburger-menu-button"
+                onClick={() => dispatch(toggleMenu())}
+              >
+                <motion.span
+                  animate={menuOpened ? { opacity: 0 } : { opacity: 1 }}
+                ></motion.span>
+                <motion.span
+                  className="middle-bar"
+                  animate={
+                    menuOpened
+                      ? {
+                          transform: "rotate(45deg)",
+                          transition: { delay: 0.2 },
+                        }
+                      : {}
+                  }
+                ></motion.span>
+                <motion.span
+                  className="middle-bar"
+                  animate={menuOpened ? { transform: "rotate(-45deg)" } : {}}
+                ></motion.span>
+                <motion.span
+                  animate={menuOpened ? { opacity: 0 } : { opacity: 1 }}
+                ></motion.span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <Menu opened={menuOpened} />
+    </>
   );
 };
 
