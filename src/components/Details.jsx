2e487680d971c880ef3b41090f5c2401171f7389
jsxgod <smykoo7@gmail.com>
2021-11-28 22:13:16 +0100

    Make use of the Redux store to load data.

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Details = () => {
+const Details = ({ productData }) => {
   return (
     <div className="details">
       <div className="details-wrapper">
@@ -8,34 +8,12 @@ const Details = () => {
           <h5>Features and specifications</h5>
         </div>
         <ul className="details-container">
-          <li>
-            <div className="details-label">Width</div>
-            <div className="details-content">8.25"</div>
-          </li>
-          <li>
-            <div className="details-label">Length</div>
-            <div className="details-content">31.88"</div>
-          </li>
-          <li>
-            <div className="details-label">Wheels</div>
-            <div className="details-content">Toy Machine 52mm</div>
-          </li>
-          <li>
-            <div className="details-label">Wheelbase</div>
-            <div className="details-content">14"</div>
-          </li>
-          <li>
-            <div className="details-label">Deck</div>
-            <div className="details-content">Toy Machine 7-ply maple deck</div>
-          </li>
-          <li>
-            <div className="details-label">Trucks</div>
-            <div className="details-content">Rukus</div>
-          </li>
-          <li>
-            <div className="details-label">Bearings</div>
-            <div className="details-content">Shielded Pig abec 5</div>
-          </li>
+          {productData.details?.map((detail, i) => (
+            <li key={productData._id + "detail-" + i}>
+              <div className="details-label">{detail.label}</div>
+              <div className="details-content">{detail.value}</div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
