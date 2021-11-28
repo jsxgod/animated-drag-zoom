920bee9ae99108ebabeb082fcf323e4a5ba7c7cb
jsxgod <smykoo7@gmail.com>
2021-11-28 22:14:20 +0100

    Clean up imports.

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
-export {default as Header } from "./Header";
-export {default as LinkHistory } from "./LinkHistory";
-export {default as Gallery } from "./Gallery";
-export {default as Product } from "./Product";
-export {default as Details } from "./Details";
+export { default as Header } from "./Header";
+export { default as LinkHistory } from "./LinkHistory";
+export { default as Gallery } from "./Gallery";
+export { default as Product } from "./Product";
+export { default as Details } from "./Details";
+export { default as Cart } from "./Cart";
+export { default as Menu } from "./Menu";
+export { default as ProductList } from "./ProductList";
+export { default as FilterSidebar } from "./FilterSidebar";
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as HomePage } from "./HomePage";
+export { default as ProductPage } from "./ProductPage";
+export { default as ProductsPage } from "./ProductsPage";
+export { default as CartPage } from "./CartPage";
