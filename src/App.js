0784222df8d754ee5e4af4f5f6332a2c8f168a69
jsxgod <smykoo7@gmail.com>
2021-12-06 02:26:54 +0100

    Use AnimatePresence for exit animations. Wrap the whole app with Router in order for AnimatePresence to work.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { Header } from "./components";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { CartPage, HomePage, ProductPage, ProductsPage } from "./pages";
 
 import "./sass/main.scss";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
+    <div className="App">
+      <Header />
+      <AnimatePresence exitBeforeEnter>
+        <Routes key={location.pathname} location={location}>
           <Route path="/" element={<HomePage />} />
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/products/:id" element={<ProductPage />} />
           <Route path="/cart" element={<CartPage />} />
         </Routes>
-      </div>
-    </Router>
+      </AnimatePresence>
+    </div>
   );
 }
 
