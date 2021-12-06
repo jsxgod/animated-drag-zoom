daff0f4e5f92c98bbca90b953b897f4ac73015e6
jsxgod <smykoo7@gmail.com>
2021-12-06 02:23:51 +0100

    Add navigation. Add cart clearing. Add cart header. Add UI animations. Dynamically render cart items data.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,57 +1,133 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { AnimatePresence, motion } from "framer-motion";
 
 import {
   addToCart,
   safeRemoveFromCart,
   removeFromCart,
+  clearCart,
 } from "../redux/features/cart/cartSlice";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
+  const ease = [0.6, 0.05, -0.01, 0.99];
+  const navigate = useNavigate();
+
   const cartItems = useSelector((state) => state.cart.cartItems);
   const totalPrice = useSelector((state) => state.cart.totalPrice);
 
   const dispatch = useDispatch();
 
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
+  const handleRemoveItemCompletely = (item) => {
+    dispatch(removeFromCart(item));
+  };
+
+  const handleIncreaseAmount = (item) => {
+    dispatch(addToCart(item));
+  };
+
+  const handleNavigate = (route) => {
+    navigate(route);
+  };
+
+  const parentSlideVariants = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+  };
+
+  const slideVariants = {
+    initial: {
+      y: -32,
+    },
+    animate: {
+      y: 0,
+    },
+  };
+
   return (
     <div className="cart">
-      <div className="cart-items-container">
-        {cartItems?.map((item) => (
-          <div key={item._id} className="cart-item-wrapper">
-            <div className="cart-item-image-wrapper">IMAGE</div>
-            <div className="cart-item-description-wrapper">
-              <div className="cart-item-name">Board</div>
-              <div className="cart-item-brand">Toy Machine</div>
-            </div>
-            <div className="cart-item-quantity-controls-wrapper">
-              <button
-                className="control-wrapper minus"
-                onClick={() => dispatch(safeRemoveFromCart(item))}
-              >
-                -
-              </button>
-              <div className="quantity-wrapper">{item.quantity}</div>
-              <button
-                className="control-wrapper plus"
-                onClick={() => dispatch(addToCart(item))}
-              >
-                +
-              </button>
-            </div>
-            <div className="cart-item-price-wrapper">
-              {"$" + item.quantity * item.price}
-            </div>
-          </div>
-        ))}
+      <div className="cart-header">
+        <button className="clear-cart-button" onClick={handleClearCart}>
+          Clear cart
+        </button>
       </div>
-      <div className="cart-info-wrapper">
+      <AnimatePresence>
+        <motion.div
+          className="cart-items-container"
+          variants={parentSlideVariants}
+          initial="initial"
+          animate="animate"
+        >
+          {cartItems?.map((item) => (
+            <motion.div
+              key={item._id}
+              className="cart-item-wrapper"
+              variants={slideVariants}
+              layout
+            >
+              <button
+                className="delete-cart-item-button"
+                onClick={() => handleRemoveItemCompletely(item)}
+              >
+                <span></span>
+                <span></span>
+              </button>
+              <div
+                className="cart-item-image-wrapper"
+                onClick={() => handleNavigate(`/products/${item._id}`)}
+              >
+                <img src={item?.images?.main} alt="img" />
+              </div>
+              <div className="cart-item-description-wrapper">
+                <div
+                  className="cart-item-name"
+                  onClick={() => handleNavigate(`/products/${item._id}`)}
+                >
+                  {item?.name}
+                </div>
+                <div className="cart-item-brand">{item?.brand}</div>
+              </div>
+              <div className="cart-item-quantity-controls-wrapper">
+                <button
+                  className="control-wrapper minus"
+                  onClick={() => dispatch(safeRemoveFromCart(item))}
+                >
+                  -
+                </button>
+                <div className="quantity-wrapper">{item.quantity}</div>
+                <button
+                  className="control-wrapper plus"
+                  onClick={() => handleIncreaseAmount(item)}
+                >
+                  +
+                </button>
+              </div>
+              <div className="cart-item-price-wrapper">
+                {"$" + item.quantity * item.price}
+              </div>
+            </motion.div>
+          ))}
+        </motion.div>
+      </AnimatePresence>
+      <motion.div
+        className="cart-info-wrapper"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.1 }}
+      >
         <div className="cart-total-price-wrapper">
           <p>Total Price: {"$" + totalPrice}</p>
         </div>
         <div className="buy-button-wrapper">
           <button className="buy-button">BUY</button>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
