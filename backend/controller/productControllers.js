06681bae34748b0fd18701aae88e288e9d826315
jsxgod <smykoo7@gmail.com>
2021-12-07 23:57:34 +0100

    Add products filtering functionality on the backend.

--- backend/controller/productControllers.js
+++ backend/controller/productControllers.js
@@ -2,14 +2,21 @@ const Product = require("../models/Product");
 
 const getAllProducts = async (req, res) => {
   try {
-    const products = await Product.find({});
+    const params = req.query;
+    for (const key of Object.keys(params)) {
+      if (params[key] === "") {
+        delete params[key];
+      }
+    }
+    console.log(params);
+    const products = await Product.find(params);
 
     res.json(products);
   } catch (error) {
     console.error(error);
-    res.status(500).json({message: "Server Error"});
+    res.status(500).json({ message: "Server Error" });
   }
-}
+};
 
 const getProduct = async (req, res) => {
   try {
@@ -18,11 +25,11 @@ const getProduct = async (req, res) => {
     res.json(product);
   } catch (error) {
     console.error(error);
-    res.status(500).json({message: "Server Error"});
+    res.status(500).json({ message: "Server Error" });
   }
-}
+};
 
 module.exports = {
   getAllProducts,
-  getProduct
-}
+  getProduct,
+};
