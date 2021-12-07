d3b949fb440d7d0008e128ebde01e4c7626599b8
jsxgod <smykoo7@gmail.com>
2021-12-07 23:54:14 +0100

    Add mock products

--- backend/data/products.js
+++ backend/data/products.js
@@ -7,8 +7,8 @@ const products = [
       "Take a ride and start rollin right with the Toy Machine Cat Monster Complete 8.25 x 31.88. This is a high quality complete that's perfect for beginning skateboarders, smaller riders, or yougnsters looking to start skateboarding. Features a top quality Toy Machine 7-ply maple deck, Rukus trucks, Toy Machine 52mm wheels and shielded Toy Machine abec 5 bearings. Fully assembled and ready to ride right out of the box.",
     price: 99,
     images: {
-      large: "https://i.imgur.com/4jwWKS6.png",
-      back: "https://i.imgur.com/2TV1sqM.png",
+      drag: "https://i.imgur.com/4jwWKS6.png",
+      main: "https://i.imgur.com/KxxCUF7.png",
       closeUp: "https://i.imgur.com/UNqoruT.png",
       side: "https://i.imgur.com/bndKHjZ.png",
       wheel: "https://i.imgur.com/aJBCo33.png",
@@ -16,11 +16,11 @@ const products = [
     details: [
       {
         label: "Width",
-        value: "8.25",
+        value: '8.25"',
       },
       {
         label: "Length",
-        value: "31.88",
+        value: '31.88"',
       },
       {
         label: "Wheels",
@@ -28,7 +28,7 @@ const products = [
       },
       {
         label: "Wheelbase",
-        value: "14",
+        value: '14"',
       },
       {
         label: "Deck",
@@ -41,6 +41,78 @@ const products = [
       {
         label: "Bearings",
         value: "Shielded Pig abec 5",
+      },
+    ],
+  },
+  {
+    product_type: "Deck",
+    brand: "Flip",
+    name: "Flip Oliveria Blacklight Deck",
+    description:
+      "See what's in the dark when skating the Flip Oliveira Blacklight Deck 8.13 x 31.5. This is a pro quality skateboard deck constructed from traditional 7-ply maple wood with background stains that could be different than what is pictured.",
+    price: 65,
+    images: {
+      drag: "https://i.imgur.com/kX0OihV.png",
+      main: "https://i.imgur.com/unE2lkS.png",
+      front: "https://i.imgur.com/beoaNFu.png",
+      side: "https://i.imgur.com/fnjbD9F.png",
+    },
+    details: [
+      {
+        label: "Width",
+        value: '8.13"',
+      },
+      {
+        label: "Length",
+        value: '31.5"',
+      },
+      {
+        label: "Wheelbase",
+        value: '14.25"',
+      },
+      {
+        label: "Construction",
+        value: "Traditional 7-ply",
+      },
+      {
+        label: "PRO MODEL",
+        value: "Luan Oliveira",
+      },
+    ],
+  },
+  {
+    product_type: "Deck",
+    brand: "Flip",
+    name: "Flip Gonzalez Blacklight Deck",
+    description:
+      "See what's in the dark when skating the Flip Gonzalez Blacklight Deck 8.0 x 32.38. This is a pro quality skateboard deck constructed from traditional 7-ply maple wood with background stains that could be different than what is pictured.",
+    price: 65,
+    images: {
+      drag: "https://i.imgur.com/XlW6jTx.png",
+      main: "https://i.imgur.com/1rg3Glz.png",
+      front: "https://i.imgur.com/qvhl9bR.png",
+      side: "https://i.imgur.com/1shgkUQ.png",
+    },
+    details: [
+      {
+        label: "Width",
+        value: '8.0"',
+      },
+      {
+        label: "Length",
+        value: '32.38"',
+      },
+      {
+        label: "Wheelbase",
+        value: '14"',
+      },
+      {
+        label: "Construction",
+        value: "Traditional 7-ply",
+      },
+      {
+        label: "PRO MODEL",
+        value: "David Gonzalez",
       },
     ],
   },
