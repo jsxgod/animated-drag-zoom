e3ae6d967ba32b09e9807fffaf32c55e15b35ab9
jsxgod <smykoo7@gmail.com>
2021-12-08 02:54:39 +0100

    Remove unnecessary logic from the component. Change how filter data is stored. Change how filter data is updated. Dynamically render Filter Sidebar contents.

--- src/components/FilterSidebar.jsx
+++ src/components/FilterSidebar.jsx
@@ -6,59 +6,49 @@ import { fetchProducts } from "../redux/features/products/productsSlice";
 
 const FilterSidebar = ({ closeSidebarHandler }) => {
   const filterState = useSelector((state) => state.filter);
-  const [brands, setBrands] = useState([]);
-  const [productTypes, setProductTypes] = useState([]);
-  const [showConfirmButton, setShowConfirmButton] = useState(false);
-  const filterData = useRef({ brand: [], product_type: [] });
   const dispatch = useDispatch();
+  const filterParams = useRef(new URLSearchParams());
 
-  useEffect(() => {
-    if (filterState.status === "success") {
-      setBrands([...new Set(filterState.data.map((product) => product.brand))]);
-      setProductTypes([
-        ...new Set(filterState.data.map((product) => product.product_type)),
-      ]);
-    }
-  }, [filterState]);
-
-  const determineShowHideConfirmButton = () => {
-    let show = false;
-    for (const [, value] of Object.entries(filterData.current)) {
-      if (value.length > 0) {
-        show = true;
-        break;
-      }
-    }
-    return show;
-  };
-  const handleFilterProductType = (productType) => {
-    if (!filterData.current.product_type.includes(productType)) {
-      filterData.current.product_type.push(productType);
-    } else {
-      filterData.current.product_type = filterData.current.product_type.filter(
-        (x) => x !== productType
-      );
-    }
-    setShowConfirmButton(determineShowHideConfirmButton());
-  };
-  const handleFilterBrand = (brand) => {
-    if (!filterData.current.brand.includes(brand)) {
-      filterData.current.brand.push(brand);
-      setShowConfirmButton(true);
-    } else {
-      filterData.current.brand = filterData.current.brand.filter(
-        (x) => x !== brand
-      );
-    }
-    setShowConfirmButton(determineShowHideConfirmButton());
-  };
-
-  const handleFilterProducts = (data) => {
+  const handleFilterProducts = () => {
     try {
-      dispatch(fetchProducts(new URLSearchParams(data)));
+      dispatch(fetchProducts(filterParams.current));
+      console.log(filterParams.current.toString());
       closeSidebarHandler(false);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const handleAddToFilterList = (label, item) => {
+    let backendLabel = "";
+    switch (label) {
+      case "Products":
+        backendLabel = "product_type";
+        break;
+      case "Brands":
+        backendLabel = "brand";
+        break;
+      default:
+        backendLabel = "";
+        break;
+    }
+    if (filterParams.current.has(backendLabel)) {
+      // Remove key=item from params
+      if (filterParams.current.getAll(backendLabel).includes(item)) {
+        // Make a copy of all the values
+        const temp = filterParams.current.getAll(backendLabel);
+        // Remove all values for the key
+        filterParams.current.delete(backendLabel);
+        temp.forEach((x) => {
+          if (x !== item) {
+            filterParams.current.append(backendLabel, x);
+          }
+        });
+      } else {
+        filterParams.current.append(backendLabel, item);
+      }
+    } else {
+      filterParams.current.append(backendLabel, item);
     }
   };
 
@@ -78,12 +68,7 @@ const FilterSidebar = ({ closeSidebarHandler }) => {
       <div className="filter-sidebar-header-wrapper">
         <h2>Filter products</h2>
         <div className="close-button-wrapper">
-          {showConfirmButton && (
-            <FaCheck
-              className="confirm"
-              onClick={() => handleFilterProducts(filterData.current)}
-            />
-          )}
+          <FaCheck className="confirm" onClick={() => handleFilterProducts()} />
           <FaTimes
             className="close"
             onClick={() => closeSidebarHandler(false)}
@@ -91,40 +76,28 @@ const FilterSidebar = ({ closeSidebarHandler }) => {
         </div>
       </div>
       <div className="filter-sidebar-sections-container">
-        <div className="section-wrapper">
-          <h3>Products</h3>
-          <div className="section-options-container">
-            {productTypes.length !== 0 &&
-              productTypes.map((productType) => (
-                <div key={productType} className="option-wrapper">
-                  <h4>{productType}</h4>
-                  <div className="checkbox-wrapper">
-                    <input
-                      type="checkbox"
-                      onChange={() => handleFilterProductType(productType)}
-                    />
-                  </div>
-                </div>
-              ))}
-          </div>
-        </div>
-        <div className="section-wrapper">
-          <h3>Brands</h3>
-          <div className="section-options-container">
-            {brands.length !== 0 &&
-              brands.map((brand) => (
-                <div key={brand} className="option-wrapper">
-                  <h4>{brand}</h4>
-                  <div className="checkbox-wrapper">
-                    <input
-                      type="checkbox"
-                      onChange={() => handleFilterBrand(brand)}
-                    />
-                  </div>
-                </div>
-              ))}
-          </div>
-        </div>
+        {filterState.status === "success" &&
+          Object.values(filterState.sections).map((section) => (
+            <div key={section.label} className="section-wrapper">
+              <h3>{section.label}</h3>
+              <div className="section-options-container">
+                {section.data.length !== 0 &&
+                  section.data.map((item) => (
+                    <div key={item} className="option-wrapper">
+                      <h4>{item}</h4>
+                      <div className="checkbox-wrapper">
+                        <input
+                          type="checkbox"
+                          onChange={() =>
+                            handleAddToFilterList(section.label, item)
+                          }
+                        />
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          ))}
       </div>
     </motion.div>
   );
