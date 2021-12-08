f748c6443d6032b19e3b68625585956dca4bd2e3
jsxgod <smykoo7@gmail.com>
2021-12-08 18:34:34 +0100

    Organize imports. Change variable name. Add animation that plays while the products are loaded.

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect } from "react";
-import { fetchProducts } from "../redux/features/products/productsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { fetchProducts } from "../redux/features/products/productsSlice";
+import { AnimatePresence, motion } from "framer-motion";
+
+import { ReactComponent as SkateboardSvg } from "../assets/svg/Skateboard.svg";
+
 const ProductList = () => {
   const dispatch = useDispatch();
-  const products = useSelector((state) => state.products.products);
+  const productsState = useSelector((state) => state.products);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -16,22 +20,59 @@ const ProductList = () => {
   };
   return (
     <div className="product-list">
-      {products?.map((product) => (
-        <div key={product._id} className="product-list-item-wrapper">
-          <div className="product-list-item-image-wrapper">
-            <img
-              src={product.images.main}
-              alt={"main"}
-              onClick={() => handleNavigate(`/products/${product._id}`)}
-            />
+      <AnimatePresence>
+        {productsState.status === "success" ? (
+          productsState.products?.map((product) => (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, transition: { delay: 0.4 } }}
+              key={product._id}
+              className="product-list-item-wrapper"
+            >
+              <div className="product-list-item-image-wrapper">
+                <img
+                  src={product.images.main}
+                  alt={"main"}
+                  onClick={() => handleNavigate(`/products/${product._id}`)}
+                />
+              </div>
+              <div className="product-list-item-description-wrapper">
+                <div className="brand-wrapper">{product.brand}</div>
+                <div className="name-wrapper">{product.name}</div>
+                <div className="price-wrapper">{"$" + product.price}</div>
+              </div>
+            </motion.div>
+          ))
+        ) : (
+          <div className="loading-animation-wrapper">
+            <motion.div
+              className="svg-wrapper"
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{
+                scale: 1.2,
+                opacity: 1,
+                transition: {
+                  duration: 0.4,
+                  repeat: Infinity,
+                  repeatDelay: 0.2,
+                  ease: [0.6, 0.05, -0.01, 0.99],
+                },
+              }}
+              exit={{
+                scale: 1.2,
+                opacity: 1,
+                transition: {
+                  duration: 0.4,
+                  repeatDelay: 0.2,
+                  ease: [0.6, 0.05, -0.01, 0.99],
+                },
+              }}
+            >
+              <SkateboardSvg className="skateboard-svg" />
+            </motion.div>
           </div>
-          <div className="product-list-item-description-wrapper">
-            <div className="brand-wrapper">{product.brand}</div>
-            <div className="name-wrapper">{product.name}</div>
-            <div className="price-wrapper">{"$" + product.price}</div>
-          </div>
-        </div>
-      ))}
+        )}
+      </AnimatePresence>
     </div>
   );
 };
