d7d7709d5352f16c84919b5d54341fe78e212cd3
jsxgod <smykoo7@gmail.com>
2021-12-14 13:54:49 +0100

    Add Products Menu opened state to the menuSlice.

--- src/redux/features/menu/menuSlice.js
+++ src/redux/features/menu/menuSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   opened: false,
+  productsMenuOpened: false,
 };
 
 export const menuSlice = createSlice({
@@ -13,13 +14,31 @@ export const menuSlice = createSlice({
     },
     closeMenu: (state, action) => {
       state.opened = false;
+      state.productsMenuOpened = false;
     },
     toggleMenu: (state, action) => {
       state.opened = !state.opened;
+      state.productsMenuOpened = false;
+    },
+    openProductsMenu: (state, action) => {
+      state.productsMenuOpened = true;
+    },
+    closeProductsMenu: (state, action) => {
+      state.productsMenuOpened = false;
+    },
+    toggleProductsMenu: (state, action) => {
+      state.productsMenuOpened = !state.productsMenuOpened;
     },
   },
 });
 
-export const { toggleMenu, openMenu, closeMenu } = menuSlice.actions;
+export const {
+  toggleMenu,
+  openMenu,
+  closeMenu,
+  openProductsMenu,
+  closeProductsMenu,
+  toggleProductsMenu,
+} = menuSlice.actions;
 
 export default menuSlice.reducer;
