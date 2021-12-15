5e8b3718990f8094ab03e7819d9f8e16ea7af6ba
jsxgod <smykoo7@gmail.com>
2021-12-15 23:37:05 +0100

    Add size selection when available. Add form. Handle submit (slightly change how items are added to the cart).

--- src/components/ProductHeader.jsx
+++ src/components/ProductHeader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { animateScroll } from "react-scroll";
 import { useDispatch } from "react-redux";
@@ -7,6 +7,10 @@ import { TiInfoLargeOutline } from "react-icons/ti";
 import { FaArrowLeft } from "react-icons/fa";
 
 const ProductHeader = ({ productData }) => {
+  const [selectedSizeOption, setSelectedSizeOption] = useState(() => {
+    const hasSizeOptions = productData.hasOwnProperty("available_sizes");
+    return hasSizeOptions ? productData.available_sizes[0] : "";
+  });
   const navigate = useNavigate();
   // REDUX
   const dispatch = useDispatch();
@@ -17,6 +21,15 @@ const ProductHeader = ({ productData }) => {
       JSON.parse(window.localStorage.getItem("cart-items")) || [];
     localCartItems.push(productData);
     window.localStorage.setItem("cart-items", JSON.stringify(localCartItems));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleAddToCart();
+  };
+
+  const handleSelectSizeOption = (event) => {
+    setSelectedSizeOption(event.target.value);
   };
 
   return (
@@ -41,19 +54,39 @@ const ProductHeader = ({ productData }) => {
           </h1>
           <h2>{productData.name}</h2>
           <p>{productData.description}</p>
-          <div className="buttons-container">
-            <button onClick={handleAddToCart}>
-              Buy Now ${productData.price}
-            </button>
-            <TiInfoLargeOutline
-              onClick={() =>
-                animateScroll.scrollToBottom({
-                  smooth: "easeInOutQuad",
-                  duration: 1000,
-                })
-              }
-            />
-          </div>
+          <form className="add-to-cart-form" onSubmit={handleSubmit}>
+            <div className="buttons-container">
+              <button>{"Buy Now $" + productData.price}</button>
+              <TiInfoLargeOutline
+                onClick={() =>
+                  animateScroll.scrollToBottom({
+                    smooth: "easeInOutQuad",
+                    duration: 1000,
+                  })
+                }
+              />
+            </div>
+            {productData.available_sizes.length !== 0 && (
+              <>
+                <h5>Pick size</h5>
+                <div className="size-options-container">
+                  {productData.available_sizes.map((size) => (
+                    <div className="size-option-wrapper">
+                      <label>
+                        <input
+                          type="radio"
+                          value={size}
+                          checked={selectedSizeOption === size}
+                          onChange={handleSelectSizeOption}
+                        />
+                        {size}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
+          </form>
         </div>
       </div>
     </div>
