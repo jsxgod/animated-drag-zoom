dc691c7f5f5ea60c144c12ecbba3fcae2f82d1cc
jsxgod <smykoo7@gmail.com>
2021-12-17 00:00:45 +0100

    Change how products are added to the cart based on whether the item has distinct sizes.

--- src/redux/features/cart/cartSlice.js
+++ src/redux/features/cart/cartSlice.js
@@ -11,46 +11,67 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      const idx = state.cartItems.findIndex(
-        (item) => item._id === action.payload._id
-      );
-      if (idx === -1) {
+      const indexes = state.cartItems
+        .map((item, i) => (item._id === action.payload._id ? i : -1))
+        .filter((index) => index !== -1);
+      if (indexes.length === 0) {
         // Item is being added to the cart for the first time
         state.cartItems.push({ ...action.payload, quantity: 1 });
-        state.totalPrice += action.payload.price;
       } else {
-        // Item already exists in the cart
-        state.cartItems[idx].quantity += 1;
-        state.totalPrice += action.payload.price;
+        /* 
+        Items with given id already exist in the cart
+        Find the one matching the size
+        */
+        if (action.payload.selected_size === "") {
+          // No size selection => only one item with such id can exist at a time
+          state.cartItems[indexes[0]].quantity += 1;
+        } else {
+          const idx = indexes.find(
+            (index) =>
+              state.cartItems[index].selected_size.value ===
+              action.payload.selected_size.value
+          );
+          if (idx === undefined) {
+            // Item with given size is being added to the cart for the first time
+            state.cartItems.push({ ...action.payload, quantity: 1 });
+          } else {
+            state.cartItems[idx].quantity += 1;
+          }
+        }
       }
+      state.totalPrice += action.payload.price;
       state.totalQuantity += 1;
     },
     safeRemoveFromCart: (state, action) => {
-      const idx = state.cartItems.findIndex(
-        (item) => item._id === action.payload._id
-      );
+      const idx = action.payload.selected_size.hasOwnProperty("value")
+        ? state.cartItems.findIndex(
+            (item) =>
+              item._id === action.payload._id &&
+              item.selected_size.value === action.payload.selected_size.value
+          )
+        : state.cartItems.findIndex((item) => item._id === action.payload._id);
       if (idx !== -1) {
         const qty = state.cartItems[idx].quantity;
         if (qty > 1) {
           state.cartItems[idx].quantity -= 1;
         } else {
-          state.cartItems = state.cartItems.filter(
-            (item) => item._id !== action.payload._id
-          );
+          state.cartItems = state.cartItems.filter((item, i) => i !== idx);
         }
         state.totalQuantity -= 1;
         state.totalPrice -= action.payload.price;
       }
     },
     removeFromCart: (state, action) => {
-      const idx = state.cartItems.findIndex(
-        (item) => item._id === action.payload._id
-      );
+      const idx = action.payload.selected_size.hasOwnProperty("value")
+        ? state.cartItems.findIndex(
+            (item) =>
+              item._id === action.payload._id &&
+              item.selected_size.value === action.payload.selected_size.value
+          )
+        : state.cartItems.findIndex((item) => item._id === action.payload._id);
       if (idx !== -1) {
         const qty = state.cartItems[idx].quantity;
-        state.cartItems = state.cartItems.filter(
-          (item) => item._id !== action.payload._id
-        );
+        state.cartItems = state.cartItems.filter((item, i) => i !== idx);
         state.totalQuantity -= qty;
         state.totalPrice -= qty * action.payload.price;
       }
