b6cdcc3644dd75a32047b914b4abc6ae448c12aa
jsxgod <smykoo7@gmail.com>
2021-12-22 23:39:15 +0100

    Show no products for selected filter message.

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -22,33 +22,41 @@ const ProductList = () => {
     <div className="product-list">
       <AnimatePresence>
         {productsState.status === "success" ? (
-          productsState.products?.map((product) => (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, transition: { delay: 0.4 } }}
-              key={product._id}
-              className="product-list-item-wrapper"
-            >
-              <div
-                className={`product-list-item-image-wrapper ${
-                  ["Wheels", "Truck", "Hardware"].includes(product.product_type)
-                    ? "horizontal"
-                    : ""
-                }`}
+          productsState.products?.length !== 0 ? (
+            productsState.products?.map((product) => (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, transition: { delay: 0.4 } }}
+                key={product._id}
+                className="product-list-item-wrapper"
               >
-                <img
-                  src={product.images.thumbnail}
-                  alt={"main"}
-                  onClick={() => handleNavigate(`/products/${product._id}`)}
-                />
-              </div>
-              <div className="product-list-item-description-wrapper">
-                <div className="brand-wrapper">{product.brand}</div>
-                <div className="name-wrapper">{product.name}</div>
-                <div className="price-wrapper">{"$" + product.price}</div>
-              </div>
-            </motion.div>
-          ))
+                <div
+                  className={`product-list-item-image-wrapper ${
+                    ["Wheels", "Truck", "Hardware"].includes(
+                      product.product_type
+                    )
+                      ? "horizontal"
+                      : ""
+                  }`}
+                >
+                  <img
+                    src={product.images.thumbnail}
+                    alt={"main"}
+                    onClick={() => handleNavigate(`/products/${product._id}`)}
+                  />
+                </div>
+                <div className="product-list-item-description-wrapper">
+                  <div className="brand-wrapper">{product.brand}</div>
+                  <div className="name-wrapper">{product.name}</div>
+                  <div className="price-wrapper">{"$" + product.price}</div>
+                </div>
+              </motion.div>
+            ))
+          ) : (
+            <div className="error-message-wrapper">
+              <p>Sorry, we could not find products with given criteria.</p>
+            </div>
+          )
         ) : (
           <LoadingAnimation />
         )}
